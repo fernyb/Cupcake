3931b295e1d00c3a9553c3cb6a76e3445aa1f984
Fernando Barajas <fernyb@fernyb.net>
2009-07-29 06:44:50 -0700

    Removing template directory

--- vendors/cupcake/specs/template/run_specs.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . "/ztest.php";
-
-$runner = new SpecRunner();
-$runner->require_all(dirname(__FILE__) . "/test");
-$runner->setDescriptiveOutput(true);
-$runner->run();
-
-?>

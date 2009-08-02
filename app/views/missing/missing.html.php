ba69221d1690a3a5a2271abd52f4ff52dbd934c4
Fernando Barajas <fernyb@fernyb.net>
2009-08-02 08:31:00 -0700

    More Cleanup and deleting files

--- app/views/missing/missing.html.php
+++
@@ -1,3 +0,0 @@
-<h2>Controller is Missing Action</h2>
-<p><strong>Controller:</strong> <?= $this->controller->name ?></p>
-<p><strong>Action:</strong> <?= $this->controller->action ?></p>
--- public/index.php
+++ public/index.php
@@ -1,26 +1,5 @@
 <?php
-define('DIR_PATH', dirname(__FILE__));
-define("VENDOR_CUPCAKE_DIR", realpath(dirname(__FILE__) . "/../vendors/cupcake"));
-define("STYLESHEETS_PATH",   realpath(dirname(__FILE__) . "/stylesheets"));
-define("TEMP_DIR_PATH",      realpath(dirname(__FILE__) . "/../tmp"));
-
-define("ROOT_PATH", realpath(dirname(__FILE__) . "/../"));
-define("APP_DIR",        ROOT_PATH ."/app");
-define("CONTROLLER_DIR", APP_DIR   ."/controllers");
-define("VIEW_DIR",       APP_DIR   ."/views");
-define("HELPER_DIR",     APP_DIR   ."/helpers");
-
-
-require_once VENDOR_CUPCAKE_DIR . "/basics.php";
-require_once VENDOR_CUPCAKE_DIR . "/inflector.php";
-require_once VENDOR_CUPCAKE_DIR . "/import.php";
-require_once VENDOR_CUPCAKE_DIR . "/dispatcher.php";
-require_once VENDOR_CUPCAKE_DIR . "/helpers.php";
-require_once VENDOR_CUPCAKE_DIR . "/controller.php";
-require_once VENDOR_CUPCAKE_DIR . "/controller_exception.php";
-require_once VENDOR_CUPCAKE_DIR . "/view.php";
-require_once VENDOR_CUPCAKE_DIR . "/router.php";
-
+require_once "cupcake.php";
 
 Router::prepare(function($r){
   $r->match("/")->to(array("controller" => "application", "action" => "show"));  

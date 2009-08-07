a0a464dda643f31b8dc9f6f9f4c2dff07ab21636
Fernando Barajas <fernyb@fernyb.net>
2009-08-07 14:46:46 -0700

    adding initial files for cookie store session

---
+++ vendors/cupcake/session/cookie_store.php
@@ -0,0 +1,16 @@
+<?php
+
+class CookieStore {
+  const MAX = 4096;
+  public $cookie_options = array();
+  
+  public function __construct($session, $options=array()) {
+    
+  }
+  
+  public function restore() {
+    
+  }
+}
+
+?>
---
+++ vendors/cupcake/specs/tests/session/cookie_store.php
@@ -0,0 +1,10 @@
+<?php
+
+describe("Session, CookieStore", function(){
+  it("stores session in a cookie", function({
+    // Write test code here
+  }));
+});
+
+
+?>

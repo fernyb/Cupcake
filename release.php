36fdc036ed0b8f10e232ef2f3ebfb0a4f792a668
Fernando Barajas <fernyb@fernyb.net>
2009-08-13 23:40:47 -0700

    Adding script to create the package

--- release.php
+++ release.php
@@ -1,4 +1,18 @@
 #!/usr/bin/env php
 <?php
+
+$package_file = "vendors/package.xml";
+
+$output = `pear package-validate {$package_file}`;  
+
+echo $output;
+
+if(!preg_match("/Validation: 0 error/", $output)) {
+  exit;
+}
+
+echo "\n****************************************\n\n";
+
+echo `pear package {$package_file}`;
   
 ?>
